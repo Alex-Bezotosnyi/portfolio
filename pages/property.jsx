47416aa6ab9aa0bf05ffcb5e79a72f8ab1222859
Alex-Bezotosnyi <alexbezaua@gmail.com>
2022-12-31 15:55:31 +0200

    upd

--- pages/property.jsx
+++ pages/property.jsx
@@ -10,11 +10,7 @@ const property = () => {
             <div className="w-screen h-[60vh] relative">
                 <div className="absolute top-0 left-0 w-full h-[60vh] bg-black/70 z-10"/>
                 <img
-<<<<<<< HEAD
                     className="absolute z-1 object-cover h-full w-full inset-0"
-=======
-                    className="absolute z-1 object-cover"
->>>>>>> b3e56687219ce84aa742c97e5e89f69cca0de36f
                     src={propertyIMG.src}
                     alt="ReactProject"
                 />
@@ -33,14 +29,14 @@ const property = () => {
                         This app was built as learning project. Frontend part was built using React, Redux Toolkit, Saas Modules and Backend part using Express.js, MongoDB. This app can Add Users and Add some tweets - in the same time you can delete them or edit.
                     </p>
                     <a
-                        href="https://github.com/Alex-Bezotosnyi/Users-Tweets"
+                        href="https://github.com/Alex-Bezotosnyi/Users-Tweets__React-Redux-Express-MongoDB"
                         target="_blank"
                         rel="noreferrer"
                     >
                         <button className="px-8 py-2 mt-4 mr-8">Code</button>
                     </a>
                     <a
-                        href="https://alex-bezotosnyi.github.io/Users-Tweets/"
+                        href="https://alex-bezotosnyi.github.io/Users-Tweets__React-Redux-Express-MongoDB/"
                         target="_blank"
                         rel="noreferrer"
                     >
